62d97c533ff33930be4340e38c06af0fdf735efe
Diprati Das <dipratidas2004@gmail.com>
2025-12-18 23:37:38 +0530

    update the base url after hosting

--- controllers/public_page_controllers.js
+++ controllers/public_page_controllers.js
@@ -1,6 +1,10 @@
 // import db from "../db";
+import "dotenv/config";
 
 
 export async function homepage_data(req, res) {
-    res.render("homepage")
+
+    let api_url = process.env.BASE_API_URL;
+    api_url = api_url + "api/"
+    res.render("homepage", {"base_api_url": api_url})
 }
